e62569fe7dabc88d45bdb0bdae8f16c8334ce836
unknown <duytruyen.msc@gmail.com>
2023-08-24 15:12:35 +0700

    fix:bugs

--- src/app/components/admin/customers/customers.component.ts
+++ src/app/components/admin/customers/customers.component.ts
@@ -27,8 +27,17 @@ export class CustomersComponent implements OnInit {
     return this._isVisibleHisProfileDialog;
   }
 
+  _isVisibleSessionDialog = false;
+  set isVisibleSessionDialog(value:boolean){
+    this._isVisibleSessionDialog = value;
+  }
+  get isVisibleSessionDialog(){
+    return this._isVisibleSessionDialog;
+  }
+  
   cols: any[] = [];
   customers:any[] = [];
+  members:any[] = [];
   dataColumn:any[] = [];
   breadcrumbItem: MenuItem[];
   home: MenuItem;
@@ -39,14 +48,24 @@ export class CustomersComponent implements OnInit {
   showProfile = false;
   selectedProfile:any;
   selectedCustomer:any;
+  selectedUserId:number | null = null;
+  hisCode:any;
+  hisProfiles:any[] = [];
+  sessions:any[] = [];
   synced = false;
   syncLabel = 'Đồng bộ hồ sơ HIS';
+  total = 0;
+  searchData = {
+    skip: 0,
+    take: 40,
+    keyword: '',
+  };
 
   constructor(
     private notification: NotificationService,
+    private fb: FormBuilder,
     private customerService: CustomerService,
   ) {
-
     this.breadcrumbItem = [
       { label: 'Quản lý khách hàng' },
       { label: 'Danh sách khách hàng' },
@@ -59,13 +78,13 @@ export class CustomersComponent implements OnInit {
    }
   ngOnInit(): void {
     this.cols = [
-      {field: 'name', header: 'Tên khách hàng', isOpSort: true, iconSort : 0, width: '15rem'},
-      {field: 'phoneNo', header: 'SĐT', isOpSort: false, iconSort : 0, width: '10rem'},
+      {field: 'name', header: 'Họ tên', width: '15rem'},
+      {field: 'phoneNo', header: 'SĐT', width: '10rem'},
       {field: 'transformedDob', header: 'Ngày sinh', width: '10rem'},
       {field: 'address', header:'Địa chỉ', width: '25rem'},
-      {field: 'patientCode', header: 'Mã BN', isOpSort: true, iconSort : 0, width: '10rem'},
-      {field: 'status', header: 'Trạng thái', isOpSort: true, iconSort : 0, width: '10rem'},
-      {field: 'registerDate', header: 'Ngày đăng kí', isOpSort: false, iconSort : 0, width: '10rem'},
+      {field: 'patientCode', header: 'Mã BN', width: '10rem'},
+      {field: 'syncStatus', header: 'Trạng thái', width: '10rem'},
+      {field: 'registerDate', header: 'Ngày đăng kí', width: '10rem'},
     ]
     this.getAll();
   }
@@ -73,10 +92,11 @@ export class CustomersComponent implements OnInit {
     this.loading = true;
     this.customerService.getAll().subscribe ({
       next: (res) => {
-        if (res && res.customers) {
+        if (res ) {
           this.customers = res.customers.map((customer: any) => ({
           ...customer,
           genderName: customer.gender === 0 ? 'Nữ' : 'Nam',
+          syncStatus: customer.isSync == true ? "Đã đồng bộ" : "Chưa đồng bộ",
           transformedDob: new DatePipe('en-US').transform(customer.dob, 'dd/MM/yyyy'),
           registerDate: new DatePipe('en-US').transform(customer.dateCreated, 'dd/MM/yyyy')
           }));
@@ -97,26 +117,122 @@ export class CustomersComponent implements OnInit {
   selectItem(customer: any){
     this.selectedCustomer = customer;
     this.onShowProfile(customer);
+    this.hisCode = customer.patientCode;
+    this.synced = customer.isSync;
+    this.sessions = [];
+    this.selectedCustomer = null;
   }
   onShowProfile(rowData:any){
-    console.log('clicked');
-    this.selectedProfile = rowData;
     this.isVisibleProfileDialog = true;
+    this.selectedProfile = rowData;
+    const selectedUserId = rowData.userId;
+    this.members = this.customers.filter(customer => customer.userId === selectedUserId);
 }
   showCustomerCard(customer:any){
     this.selectedCustomer = customer;
     this.showCard = !this.showCard;
   }
   syncProfile(){
+    this.loading = true;
+    this.customerService.getInfo(this.hisCode).subscribe({
+      next: (res) => {
+        if(res) {
+          this.hisProfiles = res;
+        } else{
+          if(res.errors && res.errors.length > 0){
+            res.errors.forEach((el: any) => {
+              this.notification.error(el.errorMessage)
+            })
+          }else{
+            this.notification.error('Lấy dữ liệu không thành công')
+          }
+        }
+      }
+    });
     this.isVisibleProfileDialog = false;
     this.isVisibleHisProfileDialog = true;
-    this.synced = !this.synced;
-    this.syncLabel = this.synced ? "Bỏ đồng bộ HIS" : "Đồng bộ hồ sơ HIS";
   }
-  updateProfile(){
+  syncOffProfile(id:any){}
+
+  updateProfile(profile:any){
+    this.loading = true;
+    this.customerService.sync(this.hisCode,this.selectedProfile.id).subscribe({
+      next: (res) => {
+        console.log(res);
+        if(res.ret[0].message){
+          this.notification.warn(res.ret[0].message);
+          this.members.push(profile);
+          this.selectedCustomer = profile;
+          console.log(this.selectedProfile);
+          this.synced = this.selectedProfile.isSync;
+          this.syncLabel = this.synced ? "Bỏ đồng bộ HIS" : "Đồng bộ hồ sơ HIS";
+        } else{
+          if(res.errors && res.errors.length > 0){
+            res.errors.forEach((el: any) => {
+              this.notification.error(el.errorMessage)
+            })
+          }else{
+            this.notification.error('Lấy dữ liệu không thành công')
+          }
+        }
+      }
+    });
     this.isVisibleHisProfileDialog = false;
     this.isVisibleProfileDialog = true;
   }
+  showSession(){
+    this.loading = true;
+    this.customerService.getSession(this.selectedProfile.id).subscribe({
+      next: (res) => {
+        if(res){
+          console.log('session', res);  
+          res.sort((a:any, b:any) => new Date(b.visitDate).getTime() - new Date(a.visitDate).getTime());       
+          this.sessions = res;
+        } else{
+          if(res.errors && res.errors.length > 0){
+            res.errors.forEach((el: any) => {
+              this.notification.error(el.errorMessage)
+            })
+          }else{
+            this.notification.error('Lấy dữ liệu không thành công')
+          }
+        }
+      }
+    });
+    this.isVisibleSessionDialog = true;
+    this.isVisibleProfileDialog = false;
+  }
+  search() {
+    this.loading = true;
+    this.customerService.search(this.searchData).subscribe({
+      next:(res) => {
+        if(res){
+          this.customers = res.customers;
+          this.total = res.total;
+        }else{
+          if( res.errors && res.errors.length > 0){
+            res.errors.forEach((el: any) => {
+              this.notification.error(el.errorMessage)
+            })
+          }else{
+            this.notification.error('Tìm kiếm khách hàng không thành công')
+            }
+          }
+        },
+      })
+      .add(() => {
+        this.loading = false;
+      });
+    }
+  resetSearch() {
+    this.searchData = {
+        skip: 0,
+        take: 40,
+        keyword: '',
+    };
+    this.search();
+  }
+  
   stopPropagation(event: Event) {
     event.stopPropagation();
   }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base-service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService extends BaseService {
   override url = '/Customer';
+  override search(searchData: any): Observable<any> {
+    return this.get(this.url, searchData);
+  }
+  getInfo(code:any):Observable<any>{
+    return this.get(`${this.url}/CheckInfo/${code}`);
+  }
+  sync(code:any,id:any):Observable<any>{
+    return this.get(`${this.url}/Sync/${code}/${id}`);
+  }
+  getSession(id:any):Observable<any>{
+    return this.get(`${this.url}/GetVisits/${id}`);
+  }
 }
